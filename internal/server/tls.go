ae48a97276e3e24e21354fcdae8249e6a451bf8e
Jiho Song <jihojiho2003@gmail.com>
2025-10-19 16:49:45 +0900

    feat(internal/server): tls

---
+++ internal/server/tls.go
@@ -0,0 +1,102 @@
+package server
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	_ "embed"
+	"fmt"
+	"os"
+
+	"network-tunneler/internal/config"
+	"network-tunneler/pkg/logger"
+)
+
+//go:embed certs/server.crt
+var embeddedCert []byte
+
+//go:embed certs/server.key
+var embeddedKey []byte
+
+//go:embed certs/ca.crt
+var embeddedCA []byte
+
+type TLSManager struct {
+	cfg    *config.TLSConfig
+	logger logger.Logger
+}
+
+func NewTLSManager(cfg *config.TLSConfig, log logger.Logger) *TLSManager {
+	return &TLSManager{
+		cfg:    cfg,
+		logger: log.With(logger.String("component", "tls")),
+	}
+}
+
+func (t *TLSManager) LoadConfig() (*tls.Config, error) {
+	cert, err := t.loadCertificate()
+	if err != nil {
+		return nil, err
+	}
+
+	caPool, err := t.loadCAPool()
+	if err != nil {
+		return nil, err
+	}
+
+	tlsConfig := &tls.Config{
+		Certificates:       []tls.Certificate{cert},
+		MinVersion:         tls.VersionTLS13,
+		InsecureSkipVerify: t.cfg.InsecureSkipVerify,
+		ClientAuth:         tls.RequireAndVerifyClientCert,
+		ClientCAs:          caPool,
+		RootCAs:            caPool,
+	}
+
+	return tlsConfig, nil
+}
+
+func (t *TLSManager) loadCertificate() (tls.Certificate, error) {
+	if t.cfg.CertPath != "" && t.cfg.KeyPath != "" {
+		t.logger.Debug("loading TLS certificate from files",
+			logger.String("cert", t.cfg.CertPath),
+			logger.String("key", t.cfg.KeyPath),
+		)
+		cert, err := tls.LoadX509KeyPair(t.cfg.CertPath, t.cfg.KeyPath)
+		if err != nil {
+			return tls.Certificate{}, fmt.Errorf("failed to load server certificate: %w", err)
+		}
+		return cert, nil
+	}
+
+	t.logger.Debug("loading TLS certificate from embedded data")
+	cert, err := tls.X509KeyPair(embeddedCert, embeddedKey)
+	if err != nil {
+		return tls.Certificate{}, fmt.Errorf("failed to load embedded server certificate: %w", err)
+	}
+	return cert, nil
+}
+
+func (t *TLSManager) loadCAPool() (*x509.CertPool, error) {
+	var caPEM []byte
+	var err error
+
+	if t.cfg.CAPath != "" {
+		t.logger.Debug("loading CA certificate from file",
+			logger.String("ca", t.cfg.CAPath),
+		)
+		caPEM, err = os.ReadFile(t.cfg.CAPath)
+		if err != nil {
+			return nil, fmt.Errorf("failed to read CA certificate: %w", err)
+		}
+	} else {
+		t.logger.Debug("loading CA certificate from embedded data")
+		caPEM = embeddedCA
+	}
+
+	caPool := x509.NewCertPool()
+	if !caPool.AppendCertsFromPEM(caPEM) {
+		return nil, fmt.Errorf("failed to parse CA certificate")
+	}
+
+	return caPool, nil
+}
---
+++ internal/server/tls_test.go
@@ -0,0 +1,79 @@
+package server
+
+import (
+	"crypto/tls"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"network-tunneler/internal/config"
+	testutil "network-tunneler/internal/testing"
+)
+
+func TestTLSManager_LoadConfig_Embedded(t *testing.T) {
+	cfg := &config.TLSConfig{
+		CertPath:           "",
+		KeyPath:            "",
+		CAPath:             "",
+		InsecureSkipVerify: false,
+	}
+
+	log := testutil.NewTestLogger()
+
+	mgr := NewTLSManager(cfg, log)
+
+	tlsConfig, err := mgr.LoadConfig()
+	if err != nil {
+		t.Fatalf("LoadConfig failed: %v", err)
+	}
+
+	if len(tlsConfig.Certificates) != 1 {
+		t.Errorf("expected 1 certificate, got %d", len(tlsConfig.Certificates))
+	}
+
+	if tlsConfig.MinVersion != tls.VersionTLS13 {
+		t.Errorf("expected TLS 1.3, got %d", tlsConfig.MinVersion)
+	}
+
+	if tlsConfig.ClientCAs == nil {
+		t.Error("expected ClientCAs to be set")
+	}
+}
+
+func TestTLSManager_LoadConfig_FromFiles(t *testing.T) {
+	tmpDir := t.TempDir()
+
+	certPath := filepath.Join(tmpDir, "test.crt")
+	keyPath := filepath.Join(tmpDir, "test.key")
+	caPath := filepath.Join(tmpDir, "ca.crt")
+
+	if err := os.WriteFile(certPath, embeddedCert, 0644); err != nil {
+		t.Fatalf("failed to write cert: %v", err)
+	}
+	if err := os.WriteFile(keyPath, embeddedKey, 0600); err != nil {
+		t.Fatalf("failed to write key: %v", err)
+	}
+	if err := os.WriteFile(caPath, embeddedCA, 0644); err != nil {
+		t.Fatalf("failed to write CA: %v", err)
+	}
+
+	cfg := &config.TLSConfig{
+		CertPath:           certPath,
+		KeyPath:            keyPath,
+		CAPath:             caPath,
+		InsecureSkipVerify: false,
+	}
+
+	log := testutil.NewTestLogger()
+
+	mgr := NewTLSManager(cfg, log)
+
+	tlsConfig, err := mgr.LoadConfig()
+	if err != nil {
+		t.Fatalf("LoadConfig failed: %v", err)
+	}
+
+	if len(tlsConfig.Certificates) != 1 {
+		t.Errorf("expected 1 certificate, got %d", len(tlsConfig.Certificates))
+	}
+}
